d12d8b68cb658b39015ee2adfd4698fb6202eeec
devKyoun <gkehdrbs36@naver.com>
2024-03-06 08:46:32 +0900

    Solve Easy Problem

---
+++ Concept/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class Text
+    {
+        static void Main(String[] args)
+        {
+            WriteLine("여러분, 안녕하세요?\n반갑습니다!");
+        }
+
+    }
+}
