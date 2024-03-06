c7928ca162d936621f6a1bffe6588f44cafee725
devKyoun <gkehdrbs36@naver.com>
2024-03-06 16:41:20 +0900

    RectArea

---
+++ Concept/RectArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RectArea
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            int rectArea = int.Parse(width) * int.Parse(height);
+            Console.WriteLine("사각형의 넓이 : {0}", rectArea);
+        }
+    }
+}
