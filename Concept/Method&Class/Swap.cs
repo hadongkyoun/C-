aef835f51a09c1fe561e52c0cee2b92a1609f956
devKyoun <gkehdrbs36@naver.com>
2024-03-14 15:37:25 +0900

    change route, swap method

---
+++ Concept/Method&Class/Swap.cs
@@ -0,0 +1,26 @@
+﻿namespace Swap
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int y = 5;
+            
+            Console.WriteLine($"x : {x}, y : {y}");
+
+            Swap(ref x, ref y);
+
+            Console.WriteLine($"x : {x}, y : {y}");
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
