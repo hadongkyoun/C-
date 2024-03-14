2f9b70aa78cb3bc0e5c42d6b7bbc48e88b45a685
devKyoun <gkehdrbs36@naver.com>
2024-03-14 16:47:46 +0900

    Find Square area

---
+++ Concept/Method&Class/Square.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Swap
+{
+    class Square
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요.: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("결과 : {0}", SquareArea(a));
+            
+
+        }
+
+         static double SquareArea(double arg)
+        {
+            return arg*arg;
+        }
+    }
+}
